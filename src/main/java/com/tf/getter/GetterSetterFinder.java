a72eda8af29774c90cfd6ede258a7f695ddfaae8
jhLee <jhLee@192.168.4.234>
2013-03-22 20:35:15 +0900

    이렇게 하는거 맞나?

--- src/main/java/com/tf/getter/GetterSetterFinder.java
+++ src/main/java/com/tf/getter/GetterSetterFinder.java
@@ -23,51 +23,30 @@ public class GetterSetterFinder {
 
 	public static void main(String[] args) throws Exception {
 		List<JavaClass> classList = new ArrayList<JavaClass>();
-		List<String> packageNames = new ArrayList<String>();
+		List<MethodInfo> overriddenMethodList = new ArrayList<MethodInfo>();
+		List<MethodInfo[]> getSetList = new ArrayList<MethodInfo[]>();
+		List<String> packageNameList = new ArrayList<String>();
+		List<String> classNameList = new ArrayList<String>();
+		List<String> methodNameList = new ArrayList<String>();
 		GetterSetterFinder.findClasses(args[0], classList);
-		for (JavaClass jc : classList) {
-			if (jc.isInterface()) {
-			} else {
-				printPureGetSetters(jc, packageNames);
-			}
-		}
-		System.out.println("Total # of classes : " + classesTotal);
-		System.out.println("Total # of methods : " + methodsTotal);
-		System.out
-				.println("Total # of pure getter/setters : " + getsetterTotal);
-		System.out
-				.println("------getter/setters methods were classified as package.------");
+		findSubClass(classList, overriddenMethodList);
+		findPureGetterSetter(classList, getSetList);
 
-		sortPackage(packageNames);
-	}
-
-	private static void printPureGetSetters(JavaClass jc,
-			List<String> packageNames) {
-		List<Method> getsetters = new ArrayList<Method>();
-		Method[] methods = jc.getMethods();
-		for (int i = 0; i < methods.length; i++) {
-			Method method = methods[i];
-			methodsTotal++;
-			if (!method.isAbstract()) {
-				if (isPureSetter(method) || isPureGetter(method)) {
-					if (isOnlygetter(methods, method)) {
-						getsetterTotal++;
-						getsetters.add(method);
-					}
-				}
-			}
+		for (int i = 0; i < getSetList.size(); i++) {
+			// System.out.println(getSetList.get(i).jc.getClassName());
+			// System.out.println(getSetList.get(i).m.getName());
 		}
-		if (getsetters.size() > 0) {
-			System.out.println("Class : " + jc.getClassName());
-			for (Method m : getsetters) {
-				packageNames.add(jc.getPackageName());
-				System.out.println("\t" + m.getName());
-			}
+
+		for (int i = 0; i < overriddenMethodList.size(); i++) {
+			// System.out.println(overriddenMethodList.get(i).jc.getClassName());
+			// System.out.println(overriddenMethodList.get(i).m.getName());
 		}
 	}
 
 	private static void sortPackage(List<String> packageNames) {
 		List<String> listTemp = new ArrayList<String>();
+		System.out
+				.println("--------------------Package 별로 분류--------------------");
 		for (int i = 0; i < packageNames.size(); i++) {
 			String pkgName = packageNames.get(i);
 			String count = String.format("%010d",
@@ -84,41 +63,96 @@ public class GetterSetterFinder {
 		}
 	}
 
-	private static boolean isOnlygetter(Method[] methods, Method method) {
-		boolean result = false;
-		String methodName = method.getName();
-		for (int i = 0; i < methods.length; i++) {
-			String compareName = methods[i].getName();
-			if (methodName.startsWith("is")) {
-				if (compareName.startsWith("get")
-						|| compareName.startsWith("set")) {
-					if (methodName.substring(2)
-							.equals(compareName.substring(3))) {
-						result = true;
-					}
-				} else if (compareName.startsWith("is")) {
-					if (methodName.substring(3)
-							.equals(compareName.substring(2))) {
-						result = true;
-					}
+	private static void sortClass(List<String> classNames,
+			List<MethodInfo> methodNames) {
+		List<String> listTemp = new ArrayList<String>();
+		System.out
+				.println("--------------------Class 별로 분류--------------------");
+		for (int i = 0; i < classNames.size(); i++) {
+			String pkgName = classNames.get(i);
+			String count = String.format("%010d",
+					Collections.frequency(classNames, pkgName));
+			listTemp.add(count + classNames.get(i));
+		}
+		ArrayList<String> sortResult = new ArrayList<String>(
+				new HashSet<String>(listTemp));
+		Collections.sort(sortResult);
+		Collections.reverse(sortResult);
+		for (int j = 0; j < sortResult.size(); j++) {
+			int count = Integer.valueOf(sortResult.get(j).substring(0, 10));
+			System.out.println(sortResult.get(j).substring(10) + " : " + count);
+			for (MethodInfo mi : methodNames) {
+				if (mi.jc.getClassName()
+						.equals(sortResult.get(j).substring(10))) {
+					System.out.println("\t" + mi.m.getName());
 				}
 			}
-			if (!methodName.equals(compareName)) {
-				if (compareName.startsWith("get")
-						|| compareName.startsWith("set")) {
-					if (methodName.substring(3)
-							.equals(compareName.substring(3))) {
-						result = true;
-					}
-				} else if (compareName.startsWith("is")) {
-					if (methodName.substring(3)
-							.equals(compareName.substring(2))) {
-						result = true;
+
+		}
+	}
+
+	private static void findPureGetterSetter(List<JavaClass> classList,
+			List<MethodInfo[]> getSetList) {
+		for (JavaClass jc : classList) {
+			Method[] methods = jc.getMethods();
+			for (Method method : methods) {
+				if (isPureSetter(method) || isPureGetter(method)) {
+					Method method2 = isOnlyGetter(methods, method);
+					if (method2 != null) {
+						System.out.println(method2);
+						System.out.println(method);
+						MethodInfo mi = new MethodInfo();
+						mi.jc = jc;
+						mi.m = method;
 					}
 				}
 			}
 		}
-		return result;
+	}
+
+	private static void findOverriddenMethod(JavaClass subClass,
+			JavaClass superClass, List<MethodInfo> overriddenMethodList) {
+		Method[] subMethods = subClass.getMethods();
+		Method[] superMethods = superClass.getMethods();
+		for (Method subMethod : subMethods) {
+			for (Method superMethod : superMethods) {
+				if (subMethod.getName().equals(superMethod.getName())) {
+					if (subMethod.getSignature().equals(
+							superMethod.getSignature()))
+						if (!subMethod.getName().contains("<init>")
+								|| !superMethod.getName().contains("<init>")) {
+							MethodInfo miSuper = new MethodInfo();
+							MethodInfo miSub = new MethodInfo();
+							miSuper.jc = superClass;
+							miSuper.m = superMethod;
+							miSub.jc = subClass;
+							miSub.m = subMethod;
+							overriddenMethodList.add(miSuper);
+							overriddenMethodList.add(miSub);
+						}
+				}
+			}
+		}
+	}
+
+	private static void findSuperClass(List<JavaClass> classList,
+			JavaClass subClass, List<MethodInfo> overriddenMethodList) {
+		String superClassName = subClass.getSuperclassName();
+		for (JavaClass superClass : classList) {
+			if (superClassName.equals(superClass.getClassName())) {
+				findOverriddenMethod(subClass, superClass, overriddenMethodList);
+			}
+		}
+	}
+
+	private static void findSubClass(List<JavaClass> classList,
+			List<MethodInfo> overriddenMethodList) {
+		for (JavaClass jc : classList) {
+			String superClassName = jc.getSuperclassName();
+			if (!superClassName.equals("java.lang.Object")) {
+				findSuperClass(classList, jc, overriddenMethodList);
+			}
+		}
 	}
 
 	static void findClasses(String dirOrZipPath, List<JavaClass> methodList)
@@ -191,7 +225,7 @@ public class GetterSetterFinder {
 
 	private static boolean isPureGetter(Method m) {
 		boolean result = false;
-		if (!m.isNative() && !m.isVolatile()) {
+		if (!m.isNative() && !m.isVolatile() && !m.isAbstract()) {
 			Code code = m.getCode();
 			int codeLength = code.getCode().length;
 			int maxStack = code.getMaxStack();
@@ -199,19 +233,25 @@ public class GetterSetterFinder {
 			Type returnType = m.getReturnType();
 			if (returnType != Type.VOID) {
 				if (returnType == Type.DOUBLE || returnType == Type.LONG) {
-					if (codeLength == 5 && maxStack == 2 && maxLocal == 1
+					boolean isPureGetterBytecodePatternBig = codeLength == 5
+							&& maxStack == 2 && maxLocal == 1;
+					if (isPureGetterBytecodePatternBig
 							&& m.getName().startsWith("get")) {
 						result = true;
 					}
-				} else if (returnType == Type.BOOLEAN) {
-					if (codeLength == 5 && maxStack == 1 && maxLocal == 1) {
-						if (m.getName().startsWith("is")) {
+				} else {
+					boolean isPureGetterBytecodePattern = codeLength == 5
+							&& maxStack == 1 && maxLocal == 1;
+					if (returnType == Type.BOOLEAN) {
+						if (isPureGetterBytecodePattern) {
+							if (m.getName().startsWith("is")) {
+								result = true;
+							}
+						}
+					} else if (isPureGetterBytecodePattern) {
+						if (m.getName().startsWith("get")) {
 							result = true;
 						}
-					}
-				} else if (codeLength == 5 && maxStack == 1 && maxLocal == 1) {
-					if (m.getName().startsWith("get")) {
-						result = true;
 					}
 				}
 			}
@@ -232,16 +272,62 @@ public class GetterSetterFinder {
 			if (argType.length == 1 && Type.VOID == returnType) {
 				if (m.getName().startsWith("set")) {
 					if (Type.DOUBLE == argType[0] || Type.LONG == argType[0]) {
-						if (codeLength == 6 && maxStack == 3 && maxLocal == 3) {
+						boolean isPureSetterBytecodePatternBig = codeLength == 6
+								&& maxStack == 3 && maxLocal == 3;
+						if (isPureSetterBytecodePatternBig) {
 							result = true;
 						}
 					}
-					if (codeLength == 6 && maxStack == 2 && maxLocal == 2) {
+					boolean isPureSetterBytecodePattern = codeLength == 6
+							&& maxStack == 2 && maxLocal == 2;
+					if (isPureSetterBytecodePattern) {
 						result = true;
 					}
 				}
 			}
 		}
 		return result;
+	}
+
+	private static Method isOnlyGetter(Method[] methods, Method method) {
+		Method resultMethod = null;
+		String methodName = method.getName();
+		for (int i = 0; i < methods.length; i++) {
+			String compareName = methods[i].getName();
+			if (methodName.startsWith("is")) {
+				if (compareName.startsWith("get")
+						|| compareName.startsWith("set")) {
+					if (methodName.substring(2)
+							.equals(compareName.substring(3))) {
+						resultMethod = methods[i];
+					}
+				} else if (compareName.startsWith("is")) {
+					if (methodName.substring(3)
+							.equals(compareName.substring(2))) {
+						resultMethod = methods[i];
+					}
+				}
+			}
+			if (!methodName.equals(compareName)) {
+				if (compareName.startsWith("get")
+						|| compareName.startsWith("set")) {
+					if (methodName.substring(3)
+							.equals(compareName.substring(3))) {
+						resultMethod = methods[i];
+					}
+				} else if (compareName.startsWith("is")) {
+					if (methodName.substring(3)
+							.equals(compareName.substring(2))) {
+						resultMethod = methods[i];
+					}
+				}
+			}
+		}
+		return resultMethod;
+	}
+
+	static class MethodInfo {
+		JavaClass jc;
+		Method m;
 	}
 }
